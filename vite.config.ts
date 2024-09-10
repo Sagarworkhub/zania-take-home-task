d72577775ee1ac64d7221da918c99591eb5cf513
Sagarworkhub <sagarbarpete3@gmail.com>
2024-09-10 17:22:18 +0530

    add vite aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteAliases({
+      prefix: '@',
+    }),
+  ],
 });
